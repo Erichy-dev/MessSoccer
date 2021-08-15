ae6d4de7b423af633c2724d56e819ed6c52268bb
Erichy-dev <83208823+Erichy-dev@users.noreply.github.com>
2021-08-15 09:13:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ class SceneA extends Phaser.Scene{
     let canvasHeight = this.game.config.height;
     let canvasWidth = this.game.config.width;
     this.ball = this.add.image(canvasWidth/1.8, canvasHeight-100,'ball').setScale(0.7);
-
+    
     this.tweens.add({
       targets:this.ball,
       y: canvasHeight-50,
       ease: 'bounce',
       duration: 2000,
     });
-
+    
     this.goalPost = this.add.graphics();
-
+    
     this.goalPost.lineStyle(5, '0xffffff');
-    this.goalPost.strokeRect(canvasWidth/2.5, 100, 300, 200);
+    this.goalPost.strokePoints([{x: canvasWidth/2.5, y: 250}, {x:canvasWidth/2.5, y:100}, {x: canvasWidth/1.5, y: 100}, {x: canvasWidth/1.5, y: 250}]);
 
     this.kicked = false;
     
